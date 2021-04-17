20f87169d7816e18e9d93fc6298440d97e5be734
LukasMirbt <39515204+LukasMirbt@users.noreply.github.com>
2021-04-17 11:40:09 +0200

    minor bugfixes 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,7 @@
 import React from "react";
 import Video, { videoPaths } from "../Video/Video";
-import videoStructure from "../Media/View-from-a-blue-moon/videoStructure";
-import getSearchableSections from "../Media/getSearchableSections";
 
 const Index = () => {
-  console.log(
-    getSearchableSections(videoStructure.sections, "View from a blue moon")
-  );
   return <Video path={videoPaths[0]} />;
 };
 
